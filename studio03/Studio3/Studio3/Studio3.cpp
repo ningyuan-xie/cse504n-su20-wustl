e5d45f75f6cf612047f597cb415ad626ac7b3019
Ningyuan Xie <howard.xie25@gmail.com>
2024-08-01 00:27:54 -0400

    Update studio03

--- studio03/Studio3/Studio3/Studio3.cpp
+++ studio03/Studio3/Studio3/Studio3.cpp
@@ -4,26 +4,26 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[])
-
+// int main(int argc, char* argv[]) is the main function of the program.
+int main(int argc, char* argv[]) {
 // int argc: "argument count", the number of command-line arguments passed to the program when it is executed.
 // char* argv[]: "argument vector", an array of strings (or array of character pointers) containing the command-line arguments themselves. 
-// Each element of this array represents one command-line argument.
+// Each element of this array represents one command-line argument: 
+// the first element (argv[0]) is the name of the program itself, and the following elements (argv[1], argv[2], etc.) are the command-line arguments passed to the program.
 
-{
-	for (auto b = argv; b != 0; ++b){ 
+	// Outerloop: This loop iterates over the elements of the argv array.
+	for (auto b = argv; *b != 0; ++b){
 		// auto b = argv: This initializes a pointer b to the beginning of the argv array. 
-		// In C++, auto is used for type inference, so b will have the same type as the elements of argv, which is char*.
-		// cout << *b << endl; // This prints the value pointed to by b
-		// *b: This dereferences the pointer b, giving us the value (i.e., a pointer to a string) that b is pointing to. 
-		// In this case, it's a command-line argument passed to the program.
+		// *b: Dereferencing b (*b) gives you a "char*", which is a pointer to the first character of a C-string.
+		// It interprets the char* as a C-string and prints the string it points to
 		std::cout << "b = " << *b << std::endl; 
 
+		// Innerloop: This loop iterates over the characters of the string pointed to by b.
 		for (auto c = b; *c != '\0'; ++c) {
 			// This condition checks whether the character pointed to by c is not equal to the null character ('\0'). 
 			// In C and C++, strings are terminated by a null character, so this loop continues until it encounters the null character, indicating the end of the string.
 			// cout << *c << endl; // This prints the character pointed to by c to the standard output stream (cout) followed by a newline (endl).
-			// *c This dereferences the pointer c, giving us the character that c is pointing to
+			// *c: Dereferences the pointer c, giving us the character that c is pointing to
 			std::cout << "c = " << *c << std::endl;
 		}
 	}
